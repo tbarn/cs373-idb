9b4e329b2dc5bbce7c0bf599ea7e92d118f61c90
Carl Bai <carlbai@gmail.com>
2015-04-22 17:47:48 -0500

    Shortened Query

--- search.sql
+++ search.sql
@@ -65,13 +65,13 @@ inner join ingredients using (ingredient_id)
 inner join recipes using(cuisine_id)
 GROUP BY cuisine_id, ingredients.name, recipes.name;
 
+select cuisine_id,name from(
 select distinct cuisine_id,name,max(rank) from(
 select distinct cuisine_id, name, ts_rank(searchCuisines.document, to_tsquery('vegetable|oil')) as rank
 from searchCuisines
-where document @@ to_tsquery('vegetable|oil')
-ORDER BY ts_rank(searchCuisines.document, to_tsquery('vegetable|oil')) DESC) t1
+where document @@ to_tsquery('vegetable|oil')) t1
 group by cuisine_id,name
-order by max(rank) DESC;
+order by max(rank) DESC) t2;
 
 
 
@@ -91,10 +91,10 @@ inner join ingredients using (ingredient_id)
 inner join cuisines using (cuisine_id)
 GROUP BY recipe_id, ingredients.name, cuisines.name;
 
+select recipe_id,name from(
 select distinct recipe_id,name, max(rank) from(
 select distinct recipe_id, name, ts_rank(searchRecipes.document, to_tsquery('vegetable|oil')) as rank
 from searchRecipes
-where document @@ to_tsquery('vegetable|oil')
-ORDER BY ts_rank(searchRecipes.document, to_tsquery('vegetable|oil')) DESC) t1
+where document @@ to_tsquery('vegetable|oil')) t1
 group by recipe_id,name
-order by max(rank) DESC;
+order by max(rank) DESC) t2;	
